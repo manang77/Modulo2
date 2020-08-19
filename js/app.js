9c8f19bc16c0b14c9550cbcdfaad2aea777cf7bb
Manuel Espejo <manuel.a.espejo@gmail.com>
2020-08-19 20:36:34 +0200

    Version Final Practica Modulo 2 Bootcamp

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ document.getElementById("anonymous").checked= IsAnonymus;
 
 FIN PRIMERA PARTE DEL EJERCICIO */
 
+// Objeto con los datos de los hoteles que se pueden seleccionar
 var hoteles = {
 
     Melia: {
@@ -32,6 +33,7 @@ var hoteles = {
     }
 };
 
+//Objeto para formatear la puntuacion con estrellas
 var estrellas = {
     una: "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
     dos: "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
@@ -40,13 +42,16 @@ var estrellas = {
     cinco: "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>"
 }
 
+//Solicitar al usuario que seleccione el hotel a puntuar
 var hotelSeleccionado = prompt("Indique hotel sobre el que quiere hacer la reseña: Melia o Fuerte");
 document.getElementById("name-hotel").innerHTML = "Hotel " + hoteles[hotelSeleccionado].name;
 document.getElementById("location-hotel").innerHTML = "Ubicado en " + hoteles[hotelSeleccionado].location;
 document.getElementById("img-hotel").src = hoteles[hotelSeleccionado].foto;
 
+//Solicitar al usuario la puntuacion para el hotel seleccionado
 var rating = prompt("Indique puntuación: una, dos, tres, cuatro o cinco");
 document.getElementById("rating").innerHTML = estrellas[rating];
 
+//Solicitar al usuario confirmacion para que la reseña sea anonima
 var IsAnonymus = confirm("¿Quiere que la reseña sea anónima?");
 document.getElementById("anonymous").checked= IsAnonymus;
